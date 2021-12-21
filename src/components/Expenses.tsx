8cec0f025fa388ea167abb264d3fd267bffffddc
Chirag Narasimha Murthy <c.narasimha.murthy@accenture.com>
2021-12-21 18:42:43 +1100

    Added another way to display the expenses list in the Expenses.tsx file

--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -25,3 +25,24 @@ function Expenses(props: {
 }
 
 export default Expenses;
+
+// /**
+//  * The below function is an explanation of what the .map is doing
+//  * @param props
+//  * @constructor
+//  */
+// function ExpensesForLoop(props: {
+//   expenses: { id: string; title: string; amount: number; date: Date }[];
+// }) {
+//   let expenseObject = [];
+//   for (let i = 0; i < props.expenses.length; i++) {
+//     expenseObject.push(
+//       <ExpenseItem
+//         date={props.expenses[i].date}
+//         title={props.expenses[i].title}
+//         amount={props.expenses[i].amount}
+//       />
+//     );
+//   }
+//   return <div className="expenses">{expenseObject}</div>;
+// }
