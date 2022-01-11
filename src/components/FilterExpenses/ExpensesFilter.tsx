3c0dffb96d90a23b4fcc6191f6eb401086fb2cb8
c.narasimha.murthy <B5t-G5m-M5nster>
2022-01-11 17:08:34 +1100

    Removed the button that will update the title of the ExpenseItem.tsx

--- src/components/FilterExpenses/ExpensesFilter.tsx
+++ src/components/FilterExpenses/ExpensesFilter.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent } from "react";
 import "./ExpensesFilter.css";
 
 /**
@@ -6,7 +6,10 @@ import "./ExpensesFilter.css";
  * @param props.onChangeFilter Will allow the value selected by the user to be passed to the parent element
  * @constructor
  */
-const ExpensesFilter = (props: { onChangeFilter: Function }) => {
+const ExpensesFilter = (props: {
+  onChangeFilter: Function;
+  selectedYear: string;
+}) => {
   const dropdownChangeHandler = (event: ChangeEvent<{ value: string }>) => {
     props.onChangeFilter(event.target.value);
   };
@@ -14,7 +17,7 @@ const ExpensesFilter = (props: { onChangeFilter: Function }) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={dropdownChangeHandler}>
+        <select onChange={dropdownChangeHandler} value={props.selectedYear}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
